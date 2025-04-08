95cc87d10e2d7674be20dacf3164547aa3f25604
gbaon19 <baongqg1999@gmail.com>
2025-04-09 00:58:31 +0700

    Update Fail

--- src/main/java/org/lab8/Calculator.java
+++ src/main/java/org/lab8/Calculator.java
@@ -2,9 +2,9 @@ package org.lab8;
 
 public class Calculator implements ICalculator {
     public Integer sum(Integer a, Integer b) {
-        return 0;
+        return a/b;
     }
     public Integer sub(Integer a, Integer b) {
-        return 0;
+        return a-b;
     }
 }
