17f972959ea86cb5cbd47564ee7e86f5ab4d28b0
gbaon19 <baongqg1999@gmail.com>
2025-04-08 22:55:19 +0700

    First commit

---
+++ src/main/java/org/lab8/Calculator.java
@@ -0,0 +1,10 @@
+package org.lab8;
+
+public class Calculator implements ICalculator {
+    public Integer sum(Integer a, Integer b) {
+        return 0;
+    }
+    public Integer sub(Integer a, Integer b) {
+        return 0;
+    }
+}
---
+++ src/main/java/org/lab8/ICalculator.java
@@ -0,0 +1,6 @@
+package org.lab8;
+
+public interface ICalculator {
+    Integer sum(Integer a, Integer b);
+    Integer sub(Integer a, Integer b);
+}
